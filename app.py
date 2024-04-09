ac665d94ddc713429417d2a34edec1f1e165a19c
banksiriwat <seekaewsiriwat@gmail.com>
2024-04-09 08:45:13 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("My First Streamlit App BANK")
+    st.write("Welcome to my Streamlit app!")
+
+    # Add more Streamlit elements (e.g., sliders, buttons, etc.) here as needed
+
+if __name__ == "__main__":
+    main()
